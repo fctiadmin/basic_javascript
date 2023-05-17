311f0daf4f2d180e36185bc44c4043769e063d6e
Future Computer Training Institute <107338637+fctiadmin@users.noreply.github.com>
2023-05-17 22:51:31 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,8 @@
+// document.getElementById('demo').innerHTML="Md Zahid Hossain";
+// document.getElementsByClassName('test')[0].innerHTML="Md Rabbi";
+// document.querySelector('.test').innerHTML="Ami and apnara";
+document.querySelector('#demo').innerHTML="Hello Madaripur peoples";
+var x=50;
+var y=100;
+var total=x+y;
+document.write(total)
